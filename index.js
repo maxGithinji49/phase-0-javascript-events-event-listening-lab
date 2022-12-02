cde4571d721df7622c7f3cc7218c295d6c37faaf
Max <maxwell.githinji@student.moringaschool.com>
2022-12-02 10:37:13 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+// function addingEventListener() {
+//     const input = document.getElementById ('input');
+// function clickAlert() {
+//         alert('I was clicked');
+//     }
+// input.addEventListener ('click', clickALert);
+// }
+
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+// const sinon = require('sinon');
+
+// describe("index.js", () => {
+//   let input;
+
+//   beforeEach(function() {
+//     input = document.getElementById('input');
+//     sinon.spy(input, 'addEventListener');
+//   })
+
+//   it("binds an event listener in addingEventListener()", () => {
+//     addingEventListener();
+//     expect(input.addEventListener.called).to.be.true;
+//   })
+// })
